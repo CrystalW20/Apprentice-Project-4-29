325515f8a27f837b0ce48310d90c50f4375dcaf2
CrystalW20 <crystal@secondstreet.com>
2019-04-30 14:34:04 -0500

    added functions to js to handle button click

---
+++ app.js
@@ -0,0 +1,23 @@
+
+
+function drag() {
+  $(".drag").draggable({
+    drag: function( click, ui ) {}
+  })
+};
+
+// Event Handler for createMagnetButton
+$(".createMagnetButton").click(function(){
+  magnetCreation();
+
+});
+  // Gather the value from the User input field
+
+  function getUserInput(){
+    return $( "input[type= text]").val();
+  };
+
+  // Create the <div> to store the new magnets value
+  function magnetCreation(){
+    $("div").add(`<div>${getUserInput()}</div>`).addClass("drag").appendTo(document.body)
+  }
